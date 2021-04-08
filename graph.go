d89db81ebc91a8e2235517d2071206ad26255d6b
Jonathan Ellis <jonathan.j.ellis@gmail.com>
2021-04-08 13:25:16 +1000

    Fix typo in timestamp generation

--- graph.go
+++ graph.go
@@ -144,7 +144,7 @@ func (g graph) Process() error {
 	// "yyyy-MM-DD_HHmmss"
 	t := time.Now()
 	timestamp := fmt.Sprintf(
-		"%d-%02d-%02d_%02d%02d%02d.csv",
+		"%d-%02d-%02d_%02d%02d%02d",
 		t.Year(), t.Month(), t.Day(),
 		t.Hour(), t.Minute(), t.Second(),
 	)
